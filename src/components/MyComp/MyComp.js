b6508c29b3598a5bad8e762bc18204a91e928510
nirch <nirh2@yahoo.com>
2021-04-11 12:30:00 +0300

    MyComp demo

---
+++ src/components/MyComp/MyComp.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './MyComp.css'
+
+function MyComp(props) {
+    return (
+        <div className="c-my-comp">
+            My Comp
+        </div>
+    );
+}
+
+export default MyComp;
