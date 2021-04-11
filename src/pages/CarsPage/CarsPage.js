cdd6306c0b854c0beccddc9a1b1327c6174d66aa
nirch <nirh2@yahoo.com>
2021-04-11 13:48:55 +0300

    CarsPage renders an empty bootstrap table

--- src/pages/CarsPage/CarsPage.js
+++ src/pages/CarsPage/CarsPage.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Table } from 'react-bootstrap';
+import CarModel from '../../model/CarModel';
 
 function CarsPage(props) {
+    const [cars, setCars] = useState([
+        new CarModel("Toyota", "Yaris", 2002, 240000),
+        new CarModel("Toyota", "Corola", 2015, 115000),
+        new CarModel("Hyundai", "i30", 2010, 180000)
+    ]);
+
+
+
+
     return (
         <Container className="p-cars">
-            <h1>Cars Page</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        <td>Brand</td>
+                        <td>Model</td>
+                        <td>Year</td>
+                        <td>KM</td>
+                        <td>KM Per Year</td>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </Table>
         </Container>
     );
 }
