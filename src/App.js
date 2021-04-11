b7d1c8ee0c15a44f069516ea64a786610cafc726
nirch <nirh2@yahoo.com>
2021-04-11 13:38:27 +0300

    adding react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CarsPage from './pages/CarsPage/CarsPage';
 
 function App() {
--- src/pages/CarsPage/CarsPage.js
+++ src/pages/CarsPage/CarsPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function CarsPage(props) {
     return (
-        <div className="p-cars">
+        <Container className="p-cars">
             <h1>Cars Page</h1>
-        </div>
+        </Container>
     );
 }
 
