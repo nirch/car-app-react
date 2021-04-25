e8486ca97c97ebe13c76e76f373b259eaeefe677
nirch <nirh2@yahoo.com>
2021-04-25 10:50:17 +0300

    adding home page + switch + exact path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import CarsPage from './pages/CarsPage/CarsPage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div>
       <HashRouter>
-        <Route path="/cars"><CarsPage/></Route>
+          <Switch>
+            <Route exact path="/"><HomePage/></Route>
+            <Route exact path="/cars"><CarsPage/></Route>
+          </Switch>
       </HashRouter>
     </div>
   );
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function HomePage(props) {
+    return (
+        <div>
+            <Container>
+                <h1 className="display-1">Cars App</h1>
+                <p>Amazing car app!!</p>
+            </Container>
+        </div>
+    );
+}
+
+export default HomePage;
