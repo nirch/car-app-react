d00543ceea997070d98fbb59e4e9069446039462
nirch <nirh2@yahoo.com>
2021-04-25 10:55:30 +0300

    Adding 404 (Not Found) Page for all other routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import CarsPage from './pages/CarsPage/CarsPage';
 import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Switch>
             <Route exact path="/"><HomePage/></Route>
             <Route exact path="/cars"><CarsPage/></Route>
+            <Route path="/"><NotFoundPage/></Route>
           </Switch>
       </HashRouter>
     </div>
